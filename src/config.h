979a1969118d3deaff6385302e2e64153e90d59c
Jim Lawton <jim.lawton@gmail.com>
2013-05-16 11:15:39 +0100

    Changed to Cork lat/long and timezone.

--- src/config.h
+++ src/config.h
@@ -1,5 +1,5 @@
 //Use the following URL to find your location http://itouchmap.com/latlong.html
-#define LATITUDE    40.315418
-#define LONGITUDE -111.706143
-#define TIMEZONE -7;
+#define LATITUDE    51.893550
+#define LONGITUDE   -8.425142
+#define TIMEZONE 1;
 #define HOUR_VIBRATION true
